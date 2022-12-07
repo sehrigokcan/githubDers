6a466eca4c9a7ed159454726ee61723d6d818e83
sehrigokcan <sehrigokcan@gmail.com>
2022-12-07 22:25:32 +0100

    second version

--- GithubDers/src/githubGiris/GitHubMain.java
+++ GithubDers/src/githubGiris/GitHubMain.java
@@ -5,8 +5,9 @@ public class GitHubMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Github!");
-		System.out.println("Second..");
 		System.out.println("Hello GitHub Hello Eclipse"); 
+		System.out.println("Second version..");
+
 	}
 
 }
