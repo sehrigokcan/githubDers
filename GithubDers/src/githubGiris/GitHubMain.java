49a89b80168ee823769e874bee9ecc5c4ac1aced
sehrigokcan <sehrigokcan@gmail.com>
2022-12-07 22:12:17 +0100

    second

--- GithubDers/src/githubGiris/GitHubMain.java
+++ GithubDers/src/githubGiris/GitHubMain.java
@@ -5,6 +5,7 @@ public class GitHubMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Github!");
+		System.out.println("Second..");
 	}
 
 }
