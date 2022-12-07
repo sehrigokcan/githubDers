9daad138348922c05169fa67895a718636161ddf
sehrigokcan <sehrigokcan@gmail.com>
2022-12-07 21:47:57 +0100

    FirstCommit

---
+++ GithubDers/src/githubGiris/GitHubMain.java
@@ -0,0 +1,10 @@
+package githubGiris;
+
+public class GitHubMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Github!");
+	}
+
+}
